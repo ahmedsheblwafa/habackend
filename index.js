757c2042555143dad2a1ad9344c69200aaeaa2a8
ahmedsheblwafa <ahmedsheblwafa@gmail.com>
2022-11-19 01:54:58 +0200

    change port to 5000

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.get("/",(req,res)=>{
     res.send("hello from app ")
 })
 
-app.listen(3000,()=>{
-    console.log("listening on port 3000");
+app.listen(5000,()=>{
+    console.log("listening on port 5000");
 })
