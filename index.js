656046e4be6b0a3ebb4d415ada5ca652513fb8e6
ahmedsheblwafa <ahmedsheblwafa@gmail.com>
2022-11-19 02:01:04 +0200

    update port

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.get("/",(req,res)=>{
     res.send("hello from app ")
 })
 
-app.listen(5000,()=>{
-    console.log("listening on port 5000");
+const PORT = process.env.PORT ||5000
+
+app.listen(PORT,()=>{
+    console.log(`listening on ${PORT}`);
 })
